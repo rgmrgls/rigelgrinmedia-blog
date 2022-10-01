af4bee633e5f0ad51e11d495456bebed3b9387b6
Shri Ganesh Jha <realsgj@gmail.com>
2022-10-01 15:08:28 +0000

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from 'react';
+import Head from 'next/head'
+import '../styles/globals.scss';
+import { Layout } from '../components';
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+<NextNProgress color="red"/>
+        <Head>
+          <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" />
+          <title>RigelGrin Media Blog</title>
+        </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
